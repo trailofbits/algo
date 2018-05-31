cefbd22b4546e6c58a158aba61cff46a187549da
Jack Ivanov <e601809@gmail.com>
2018-05-31 19:32:41 +0300

    TravisCI fixes

--- tests/update-users.sh
+++ tests/update-users.sh
@@ -3,20 +3,18 @@
 set -ex
 
 CAPW=`cat /tmp/ca_password`
-USER_ARGS="server_ip=$LXC_IP server_user=ubuntu ssh_tunneling_enabled=y IP_subject=$LXC_IP easyrsa_CA_password=$CAPW"
+USER_ARGS="server_ip=$LXC_IP server_user=ubuntu ssh_tunneling_enabled=y IP_subject=$LXC_IP easyrsa_CA_password=$CAPW apparmor_enabled=false install_headers=false"
 
 sed -i 's/- jack$/- jack_test/' config.cfg
 
 if [ "${LXC_NAME}" == "docker" ]
 then
-  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -e \"${USER_ARGS}\""
+  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/algo/configs -e "USER_ARGS=${USER_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook users.yml -e \"${USER_ARGS}\" -t update-users --skip-tags common"
 else
-  ansible-playbook users.yml -e "${USER_ARGS}"
+  ansible-playbook users.yml -e "${USER_ARGS}" -t update-users --skip-tags common
 fi
 
-cd configs/$LXC_IP/pki/
-
-if openssl crl -inform pem -noout -text -in crl/jack.crt | grep CRL
+if sudo openssl crl -inform pem -noout -text -in configs/$LXC_IP/pki/crl/jack.crt | grep CRL
   then
     echo "The CRL check passed"
   else
@@ -24,7 +22,7 @@ if openssl crl -inform pem -noout -text -in crl/jack.crt | grep CRL
     exit 1
 fi
 
-if openssl x509 -inform pem -noout -text -in certs/jack_test.crt | grep CN=jack_test
+if sudo openssl x509 -inform pem -noout -text -in configs/$LXC_IP/pki/certs/jack_test.crt | grep CN=jack_test
   then
     echo "The new user exists"
   else
