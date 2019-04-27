bbb6db457d6f8685468b6ab4160f40e11ca3f2dc
johnwesley <wesley@echoboat.org>
2019-04-27 13:08:10 -0400

    merge upstream master

--- tests/local-deploy.sh
+++ tests/local-deploy.sh
@@ -6,11 +6,7 @@ DEPLOY_ARGS="provider=local server=10.0.8.100 ssh_user=ubuntu endpoint=10.0.8.10
 
 if [ "${DEPLOY}" == "docker" ]
 then
-<<<<<<< HEAD
-  make docker-test-local
-=======
   docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -v $(pwd)/configs:/algo/configs -e "DEPLOY_ARGS=${DEPLOY_ARGS}" travis/algo /bin/sh -c "chown -R root: /root/.ssh && chmod -R 600 /root/.ssh && source env/bin/activate && ansible-playbook main.yml -e \"${DEPLOY_ARGS}\" --skip-tags apparmor"
->>>>>>> upstream/master
 else
   ansible-playbook main.yml -e "${DEPLOY_ARGS}" --skip-tags apparmor
 fi
