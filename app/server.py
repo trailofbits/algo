7dd4044670b9b4e9f0d831624da223231c640a84
Ivan Gromov <summer.is.gone@gmail.com>
2019-10-15 01:35:40 +0500

    Initial webapp version

--- app/server.py
+++ app/server.py
@@ -1,12 +1,14 @@
 import asyncio
-from os.path import join, dirname
-
+import signal
+import sys
 import aiohttp
 import yaml
+from os.path import join, dirname
 from aiohttp import web
 
 routes = web.RouteTableDef()
 PROJECT_ROOT = dirname(dirname(__file__))
+jobs = []
 
 
 async def handle_index(_):
@@ -28,6 +30,7 @@ async def websocket_handler(request):
                     stdout=asyncio.subprocess.PIPE,
                     stderr=asyncio.subprocess.PIPE
                 )
+                jobs.append(p)
                 while True:
                     line = await p.stdout.readline()
                     if not line:
@@ -42,30 +45,57 @@ async def websocket_handler(request):
     return ws
 
 
-@routes.view("/config")
-class UsersView(web.View):
-    async def get(self):
-        with open(join(PROJECT_ROOT, 'config.cfg'), 'r') as f:
-            config = yaml.safe_load(f.read())
-            return web.json_response(config)
+@routes.get('/config')
+async def get_config(_):
+    with open(join(PROJECT_ROOT, 'config.cfg'), 'r') as f:
+        config = yaml.safe_load(f.read())
+        return web.json_response(config)
 
-    async def post(self):
-        data = await self.request.json()
-        with open(join(PROJECT_ROOT, 'config.cfg'), 'w') as f:
-            try:
-                config = yaml.safe_dump(data)
-            except Exception as e:
-                return web.json_response({'error': {
-                    'code': type(e).__name__,
-                    'message': e,
-                }}, status=400)
-            else:
-                f.write(config)
-                return web.json_response({'ok': True})
 
+@routes.post('/config')
+async def post_config(request):
+    data = await request.json()
+    with open(join(PROJECT_ROOT, 'config.cfg'), 'w') as f:
+        try:
+            config = yaml.safe_dump(data)
+        except Exception as e:
+            return web.json_response({'error': {
+                'code': type(e).__name__,
+                'message': e,
+            }}, status=400)
+        else:
+            f.write(config)
+            return web.json_response({'ok': True})
+
+
+@routes.post('/do/regions')
+async def get_do_regions(request):
+    data = await request.json()
+    async with aiohttp.ClientSession() as session:
+        url = 'https://api.digitalocean.com/v2/regions'
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {0}'.format(data['token']),
+        }
+        async with session.get(url, headers=headers) as r:
+            json_body = await r.json()
+            return web.json_response(json_body)
 
 app = web.Application()
 app.router.add_get('/ws', websocket_handler)
 app.router.add_get('/', handle_index)
 app.router.add_routes(routes)
 web.run_app(app)
+
+def signal_handler(sig, frame):
+    print('Closing child processes')
+    for p in jobs:
+        try:
+            p.terminate()
+        except:
+            pass
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+print('Press Ctrl+C to stop')
+signal.pause()
