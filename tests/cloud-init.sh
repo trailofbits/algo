88155e3e715b3f506830c6865f668410a09d9d2c
Jack Ivanov <17044561+jackivanov@users.noreply.github.com>
2020-01-08 11:46:36 +0100

    Github Actions

--- tests/cloud-init.sh
+++ tests/cloud-init.sh
@@ -9,9 +9,9 @@ export DNS_ADBLOCKING=true
 export SSH_TUNNELING=true
 export ENDPOINT=10.0.8.100
 export USERS=desktop,user1,user2
-export EXTRA_VARS='install_headers=false tests=true apparmor_enabled=false local_service_ip=172.16.0.1'
-export ANSIBLE_EXTRA_ARGS='--skip-tags apparmor'
-export REPO_SLUG=${TRAVIS_PULL_REQUEST_SLUG:-${TRAVIS_REPO_SLUG:-trailofbits/algo}}
-export REPO_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH:-master}}
+export EXTRA_VARS='install_headers=false tests=true local_service_ip=172.16.0.1'
+export ANSIBLE_EXTRA_ARGS=''
+export REPO_SLUG=${REPOSITORY:-trailofbits/algo}
+export REPO_BRANCH=${BRANCH:-master}
 
-curl -s https://raw.githubusercontent.com/${TRAVIS_PULL_REQUEST_SLUG:-${TRAVIS_REPO_SLUG}}/${TRAVIS_PULL_REQUEST_BRANCH:-${TRAVIS_BRANCH}}/install.sh | sudo -E bash -x"
+curl -s https://raw.githubusercontent.com/${REPOSITORY:-trailofbits/algo}/${BRANCH:-master}/install.sh | sudo -E bash -x"
--- tests/ssh-tunnel.sh
+++ tests/ssh-tunnel.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-PASS=$(grep ^p12_password: configs/10.0.8.100/.config.yml | awk '{print $2}')
+PASS=$(grep ^p12_password: configs/10.0.8.100/.config.yml | awk '{print $2}' | cut -f2 -d\')
 
 ssh-keygen -p -P ${PASS} -N '' -f configs/10.0.8.100/ssh-tunnel/desktop.pem
 
@@ -10,6 +10,6 @@ ssh -o StrictHostKeyChecking=no -D 127.0.0.1:1080 -f -q -C -N desktop@10.0.8.100
 
 git config --global http.proxy 'socks5://127.0.0.1:1080'
 
-git clone -vv https://github.com/trailofbits/algo /tmp/ssh-tunnel-check
+for i in {1..10}; do git clone -vv https://github.com/trailofbits/algo /tmp/ssh-tunnel-check && break || sleep 1; done
 
 echo "SSH tunneling tests passed"
