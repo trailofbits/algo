43633a0450849eed4d2ec9fd8a44fa379c5d47f9
Jack Ivanov <e601809@gmail.com>
2020-01-15 17:41:19 +0100

    apt locking fixes

--- files/cloud-init/base.sh
+++ files/cloud-init/base.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -eux
 
-apt-get update -y
-apt-get install sudo -y
+which sudo || until \
+  apt-get update -y && \
+  apt-get install sudo -yf --install-suggests; do
+  sleep 3
+done
 
-getent passwd algo || useradd -m -d /home/algo -s /bin/bash -G adm,netdev -p '!' algo
+getent passwd algo || useradd -m -d /home/algo -s /bin/bash -G adm -p '!' algo
 
 (umask 337 && echo "algo ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/10-algo-user)
 
@@ -15,5 +18,8 @@ EOF
 test -d /home/algo/.ssh || (umask 077 && sudo -u algo mkdir -p /home/algo/.ssh/)
 echo "{{ lookup('file', '{{ SSH_keys.public }}') }}" | (umask 177 && sudo -u algo tee /home/algo/.ssh/authorized_keys)
 
-sudo apt-get remove -y --purge sshguard || true
+dpkg -l sshguard && until apt-get remove -y --purge sshguard; do
+  sleep 3
+done || true
+
 systemctl restart sshd.service
