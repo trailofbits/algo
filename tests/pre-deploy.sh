d8c48ec505861a630a1fcdc7788ec6513b9374bb
Jack Ivanov <17044561+jackivanov@users.noreply.github.com>
2020-01-16 13:24:23 +0100

    Update pre-deploy.sh

--- tests/pre-deploy.sh
+++ tests/pre-deploy.sh
@@ -9,7 +9,8 @@ cp -f tests/lxd-bridge /etc/default/lxd-bridge
 cp -f tests/algo.conf /etc/default/algo.conf
 
 export REPOSITORY=${REPOSITORY:-${GITHUB_REPOSITORY}}
-export BRANCH=${BRANCH:-${GITHUB_REF#refs/heads/}}
+export _BRANCH=${BRANCH#refs/heads/}
+export BRANCH=${_BRANCH:-${GITHUB_REF#refs/heads/}}
 
 if [[ "$DEPLOY" == "cloud-init" ]]; then
   bash tests/cloud-init.sh | lxc profile set default user.user-data -
